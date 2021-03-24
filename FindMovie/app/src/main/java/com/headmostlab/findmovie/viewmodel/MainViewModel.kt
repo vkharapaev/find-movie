48b1108c0d6cbf5c11726032029edfa58d0600ed
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-03-24 23:06:28 +0500

    ViewModel is implemented

--- FindMovie/app/src/main/java/com/headmostlab/findmovie/viewmodel/MainViewModel.kt
+++ FindMovie/app/src/main/java/com/headmostlab/findmovie/viewmodel/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.headmostlab.findmovie.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.headmostlab.findmovie.model.Repository
+import com.headmostlab.findmovie.model.RepositoryImpl
+import java.util.concurrent.TimeUnit
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val appStateLiveData: MutableLiveData<AppState> = MutableLiveData(),
+    private val repository: Repository = RepositoryImpl()
+) :
+    ViewModel() {
+
+    fun getAppStateLiveData(): LiveData<AppState> = appStateLiveData
+
+    fun getMovies() {
+        appStateLiveData.value = AppState.Loading
+        Thread {
+            TimeUnit.SECONDS.sleep(1)
+            appStateLiveData.postValue(AppState.Success(repository.getMovies()))
+        }.start()
+    }
 }
