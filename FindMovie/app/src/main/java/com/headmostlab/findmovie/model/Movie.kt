a34750a9cdfd0fe5d549f5822318ea3efa2061d3
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-03-24 22:49:24 +0500

    id field is added to Movie model

--- FindMovie/app/src/main/java/com/headmostlab/findmovie/model/Movie.kt
+++ FindMovie/app/src/main/java/com/headmostlab/findmovie/model/Movie.kt
@@ -1,3 +1,3 @@
 package com.headmostlab.findmovie.model
 
-data class Movie(var title: String, var year: Int, var rating: Double, var poster: String)
+data class Movie(val id:Int, val title: String, val year: Int, val rating: Double, val poster: String)
