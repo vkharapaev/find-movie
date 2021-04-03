80f90ffa8de71012259a2fc5fed99fdca14df6ef
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-04-03 19:07:45 +0500

    Api Key is moved to a configuration file

--- FindMovie/app/src/main/java/com/headmostlab/findmovie/network/tmdb/TMDbApiKeyProvider.kt
+++ FindMovie/app/src/main/java/com/headmostlab/findmovie/network/tmdb/TMDbApiKeyProvider.kt
@@ -1,7 +1,8 @@
 package com.headmostlab.findmovie.network.tmdb
 
+import com.headmostlab.findmovie.BuildConfig
 import com.headmostlab.findmovie.network.ApiKeyProvider
 
-class TMDbApiKeyProvider() : ApiKeyProvider {
-    override fun getApiKey() = "bee201356b116fa4f1a0d2708e829940"
+class TMDbApiKeyProvider : ApiKeyProvider {
+    override fun getApiKey() = BuildConfig.TMDB_API_KEY
 }
