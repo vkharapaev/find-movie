ab5027bbc3c9c29c8b20586e546fa062d0d547f4
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-05-13 18:55:47 +0500

    Navigation component is embedded in the app

--- FindMovie/app/src/main/java/com/headmostlab/findmovie/ui/view/nointernet/NoInternetFragment.kt
+++ FindMovie/app/src/main/java/com/headmostlab/findmovie/ui/view/nointernet/NoInternetFragment.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.headmostlab.findmovie.R
 import com.headmostlab.findmovie.databinding.NoInternetFragmentBinding
-import com.headmostlab.findmovie.ui.view.main.MainFragment
 import com.headmostlab.findmovie.ui.view.utils.viewBinding
 
 class NoInternetFragment : Fragment(R.layout.no_internet_fragment) {
     companion object {
         const val RETRY_TO_CONNECT = "RETRY_TO_CONNECT"
-        fun newInstance() = NoInternetFragment()
     }
 
     private val binding by viewBinding(NoInternetFragmentBinding::bind)
@@ -20,7 +19,7 @@ class NoInternetFragment : Fragment(R.layout.no_internet_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.retryButton.setOnClickListener {
             parentFragmentManager.setFragmentResult("RETRY_TO_CONNECT", bundleOf())
-            parentFragmentManager.popBackStack()
+            findNavController().popBackStack()
         }
     }
 }
